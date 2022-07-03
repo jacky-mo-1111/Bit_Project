fac1b2193055935ee65f325157ea6c844de404f0
Wenjie Mo <jackymo@usc.edu>
2022-07-03 02:05:41 -0400

    2

--- hackervoice/index.js
+++ hackervoice/index.js
@@ -1,11 +1,21 @@
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
 
-    var password = req.query.password;
+
+    let result =""; 
+    var password = req.query.password; 
     context.log(password)
+
+
+    if (password == "letmein"){
+        result = "Access granted."
+    }
+    else{
+        result = "Access denied."
+    }
 
     context.res = {
         // status: 200, /* Defaults to 200 */
-        body: password
+        body: result
     };
 }
