0ca854990035c03fb8de753977601d96e2c511bd
Wenjie Mo <jackymo@usc.edu>
2022-07-07 00:03:45 -0400

    2.7.3

--- song4u/index.js
+++ song4u/index.js
@@ -1,5 +1,5 @@
 const querystring = require('qs');
-const fetch = require ("node-fetch")
+const fetch = require ("node-fetch");
 
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.');
